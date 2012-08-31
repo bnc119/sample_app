85326371f78b527acd714c481de9df569d666151
Bence Magyar <bence.magyar@gmail.com>
2012-08-31 15:23:44 -0400

    starting 8.3.3 - The flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
     @title = "Sign up"
   end
   
@@ -8,6 +9,17 @@ class UsersController < ApplicationController
     
     @user = User.find(params[:id])
     @title = @user.name
+  end
+  
+  def create
+    @user = User.new (params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+    
   end
 
 end
