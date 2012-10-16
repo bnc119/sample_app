ef0e22dfd025564e3f5c6cb72d45301bd28be01c
Bence Magyar <bence.magyar@gmail.com>
2012-10-15 22:04:11 -0400

    up-to-and-including page 375.  Need to start on 10.2 - Protecting Pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
   
+  # respond to GET users/new
   def new
     @user = User.new
     @title = "Sign up"
   end
   
+  # respond to get calls
   def show
     
     @user = User.find(params[:id])
     @title = @user.name
   end
   
+  # respond to POST /users
   def create
     @user = User.new (params[:user])
     if @user.save
@@ -23,5 +26,26 @@ class UsersController < ApplicationController
     end
     
   end
+  
+  # respond to GET /users/1/edit
+  def edit
+    @title="Edit user"
+    @user = User.find(params[:id])
+    
+  end
+  
+  #respond to PUT /users/1
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] ="Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit User"
+      render 'edit'
+    end
+      
+  end
+  
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -126,6 +126,78 @@ describe UsersController do
     end
   end
   
+  describe "GET 'edit'" do
+    before (:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    
+    
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector("title", :content => "Edit user")
+    end
+    
+    it "should have a link to Gravatar" do
+      get :edit, :id => @user
+      gravatar_url = "http://gravatar.com/emails"
+      response.should have_selector("a", :href => gravatar_url, :content => "change")
+    end
   
-
+  end
+  
+  describe "PUT 'update' " do
+    
+     before (:each) do
+       @user = Factory(:user)
+       test_sign_in(@user)
+     end
+     
+     describe "failure" do
+       before(:each) do
+        @attr = { :name=> "", :email=> "",:password => "", :password_confirmation=> "" }
+       end
+       
+       it "should render the edit page" do
+         put :update, :id => @user, :user => @attr
+         response.should render_template('edit')
+       end
+       
+       it "should have the right title" do
+         put :update, :id => @user, :user => @attr
+         response.should have_selector("title", :content => "Edit User")
+       end
+     end
+     
+     describe "success" do
+       before(:each) do
+         @attr = { :name=> "New Name", :email=> "user@example.org",
+         :password => "foobar", :password_confirmation=> "foobar" }
+       end
+       
+       it "should change the user's attributes" do
+         put :update, :id => @user, :user => @attr
+         @user.reload
+         @user.name.should == @attr[:name]
+         @user.email.should == @attr[:email]
+       end
+       
+       it "should redirect to the show users page" do
+         put :update, :id => @user, :user => @attr
+         response.should redirect_to(user_path(@user))
+       end
+       
+       it "should have a flash message" do
+         put :update, :id => @user, :user => @attr
+         flash[:success].should =~ /updated/
+       end
+       
+     end
+   end
+  
 end
