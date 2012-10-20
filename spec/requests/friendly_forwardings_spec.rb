f4553efa8137dbdbbc7f35b15f4596a7b05a0630
Bence Magyar <bence.magyar@gmail.com>
2012-10-19 23:34:38 -0400

    finished 10.2 - protecting pages and friendly forwarding

---
+++ spec/requests/friendly_forwardings_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "FriendlyForwardings" do
+  it "should forward to requested page after signin" do
+    user = Factory(:user)
+    visit edit_user_path(user)
+    # the test automatically follows the redirect to the signin page
+    fill_in :email, :with => user.email
+    fill_in :password, :with => user.password
+    click_button
+    
+    # the test follows the redirect again, this time to users/edit
+    response.should render_template('users/edit')
+    
+  end
+end
